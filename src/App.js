242e5d0bea69e6552d0940fa6ea27a8461a9a5ff
SargisCH <sargis.chilingaryann@gmail.com>
2024-10-29 17:05:39 +0400

    Initial commit
    added esprima
    code copied from textarea to div element

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [lines, setLines] = useState([]);
+  const onChangeTextarea = (e) => {
+    const text = e.target.value;
+    try {
+      const textSplitted = text.split("\n");
+      const linesClonded = [...lines];
+      textSplitted.forEach((lineCode, lineIndex) => {
+        const lineFound = lines.find((l) => l.lineNumber === lineIndex + 1);
+        if (lineFound) {
+          const newLine = { ...lineFound };
+          newLine.code = lineCode;
+          linesClonded[lineIndex] = newLine;
+        }
+      });
+
+      setLines(linesClonded);
+      const parsed = window.esprima.parse(text);
+      console.log("parsed", JSON.parse(JSON.stringify(parsed)));
+    } catch (e) {
+      console.log(e.toString(), e.message);
+    }
+  };
+  function createLines() {
+    const linesTemp = [];
+    for (let i = 0; i < 11; i++) {
+      linesTemp.push({
+        lineNumber: i + 1,
+      });
+    }
+    return linesTemp;
+  }
+  useEffect(() => {
+    setLines(createLines());
+  }, [setLines]);
+
+  return (
+    <div className="App" style={{ marginTop: "0px" }}>
+      <header className="App-header">
+        <h1>Code Editor</h1>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            gap: "10px",
+          }}
+        >
+          <textarea
+            style={{
+              margin: 0,
+              padding: "5px",
+              border: 0,
+              width: "500px",
+              height: "250px",
+              maxWidth: "500px",
+              maxheight: "250px",
+              background: "darkGrey",
+            }}
+            onChange={onChangeTextarea}
+          />
+          <div
+            style={{
+              width: "500px",
+              height: "250px",
+              background: "darkGrey",
+              padding: "5px",
+            }}
+          >
+            <div className="lines">
+              {lines.map((l) => (
+                <div className="line" key={l.lineNumber}>
+                  <div className="line-number">{l.lineNumber}.</div>
+                  <div className="line-code">{l.code}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
